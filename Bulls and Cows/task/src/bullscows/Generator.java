e7c663e64860dd8722fbd6363c239d251b84a2a5
Filip Markovic <107422386+FilipM79@users.noreply.github.com>
2022-08-19 10:03:49 +0200

    Medjufaza 1

--- Bulls and Cows/task/src/bullscows/Generator.java
+++ Bulls and Cows/task/src/bullscows/Generator.java
@@ -4,27 +4,40 @@ import java.util.*;
 
 public interface Generator {
     Random random = new Random();
-    List<Integer> secretCode = new ArrayList<>();
-    List<Integer> clonedCode = new ArrayList<>();
+    List<Character> asciiSecretCode = new ArrayList<>();
+    List<Character> secretCode = new ArrayList<>();
+    List<Character> clonedCode = new ArrayList<>();
 
 
     static void generateRandomCode(int length) {
 
         int counter = 0;
-
+        char ch1 = 0;
+//        while (counter < length) {
+//            int digit = random.nextInt(36) + 48;
+//            ch1 = (char) digit;
+//
+//            asciiSecretCode.add(counter, ch1);
+//            counter++;
+//        }
         while (counter < length) {
-            int digit = Math.abs(random.nextInt(10));
 
-            if (counter == 0) {
-                secretCode.add(counter, random.nextInt(9) + 1);
-                counter++;
-            } else if (!secretCode.contains(digit)) {
-                secretCode.add(counter, digit);
+            int digit = random.nextInt(36) + 48;
+            if (digit < 58) {
+                ch1 = (char) digit;
+            } else {
+                ch1 = (char) (digit + 39);
+            }
+
+            if (!secretCode.contains(digit)) {
+                secretCode.add(counter, (char) Integer.parseInt(String.valueOf((byte) ch1)));
                 counter++;
             }
         }
 
         clonedCode.addAll(secretCode);
         System.out.println("Okay, let's start a game!");
+        System.out.println("secretCode: " + secretCode);
+//        System.out.println("clonedCode: " + clonedCode);
     }
 }
--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -1,30 +1,52 @@
 package bullscows;
 
+import java.util.*;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Main implements Generator, User, BullsAndCows {
     public static void main(String[] args) {
-
+//        0123456789abcdefghijklmnopqrstuvwxyz
         Scanner scanner = new Scanner(System.in);
+
+//        String s1 = scanner.nextLine();
+//        String c1 = Arrays.toString(s1.getBytes());
+//        String s3 = "";
+//
+//        System.out.println("c1: " + c1);
+//
+//        for (int i = 0; i < s1.length(); i++) {
+//            int i11 = Integer.parseInt(String.valueOf((byte) s1.charAt(i)));
+//            System.out.println("i11: " + i11);
+//            char ch1 = (char) i11;
+//            System.out.println("ch1: " + ch1);
+//            int i12 = Integer.parseInt(String.valueOf((byte) ch1));
+//            System.out.println("i12: " + i12);
+//            s3 = s3.concat(String.valueOf(ch1));
+//
+//        } System.out.println("s3: " + s3);
+
 
         System.out.println("Please, enter the secret code's length:");
         System.out.print("> ");
         int length = scanner.nextInt();
-
-        if (length < 11) {
-            for (int i = 0; i < length; i++) {
-                bull.add(false);
-                cow.add(false);
-                compare.add(true);
-                checkBullIndex.add(false);
-            }
-
+//
+//        if (length < 11) {
+//            for (int i = 0; i < length; i++) {
+//                bull.add(false);
+//                cow.add(false);
+//                compare.add(true);
+//                checkBullIndex.add(false);
+//            }
+//
             Generator.generateRandomCode(length);
 
             String concatCode = "";
             for (int k = 0; k < length; k++) {
                 concatCode = concatCode.concat(String.valueOf(secretCode.get(k)));
             }
+
+        System.out.println("concatCode: " + concatCode);
 
             int counter = 1;
             User.input(counter);
@@ -38,9 +60,9 @@ public class Main implements Generator, User, BullsAndCows {
                 if (BullsAndCows.bull.equals(BullsAndCows.compare)) loop = true;
             }
             System.out.println("Congratulations! You guessed the secret code.");
-        } else {
-            System.out.println("Error: can't generate a secret number with a length of 11 " +
-                    "because there aren't enough unique digits.");
-        }
+//        } else {
+//            System.out.println("Error: can't generate a secret number with a length of 11 " +
+//                    "because there aren't enough unique digits.");
+//        }
     }
 }
